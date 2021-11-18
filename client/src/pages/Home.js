bf9447ffb81a03eade7003e58259b06cb57f9a21
stephany alde <stephany05alde@gmail.com>
2021-11-17 20:51:03 -0600

    new changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,33 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import ReactPlayer from 'react-player';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
-    </div>
-  );
-};
+function Home() {
+    return (
+        <>
+        <form>
+          <label><h1>What are you listening to?</h1></label>
+          <input type="text" id="fname" name="form"></input>
+          <button type="button">Submit</button>
+        </form>
+  
+        <div id="middle"></div>
+        <h2>Check out this new up and coming artist</h2>
+        <div class="row"></div>
+        <h3>Lyrics:</h3>
+        <p>
+          <h4>Dodie with "Hate Myself," from Build A Problem</h4>
+          "Could it be different?
+          did I ruin the day?
+          do you look angry?
+          oh, what did I say?
+          filling in the gaps
+          build a problem that
+          neither of us needs
+          something wrong with me
+          I'm getting desperate..." <h4>check out more from this song down below </h4></p>
+        <ReactPlayer url='https://www.youtube.com/watch?v=MTPeEuQ-9ys' />
+        </>
+    )
+}
 
 export default Home;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,71 +1,31 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React from 'react';
+import './login.css';
 
-function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
+function Login() {
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <form action="action_page.php" method="post">
+      <div className="heading">
+        <h1><a href="./index.html">Listen in</a></h1>
+      </div>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+        <button type="submit">Login</button>
+          <input type="checkbox" checked="checked" name="remember" />
+          <label>
+         Remember me
+        </label>
+      </div>
+
+      <div className="container" style={{backgroundColor: "rgb(147, 133, 133)"}}>
+        <button type="button" className="cancelbtn">Cancel</button>
+      </div>
+    </form>
   );
 }
-
 export default Login;
